e34944360b7a232b6385fdd55b51091a61268b32
Abdilah Ramadhani <abdilahramadhani06@gmail.com>
2023-09-02 15:39:14 +0300

    added prisma and schema

---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require("./src/server")
+
+
+
+app.listen(8700,()=>{
+
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
 const app = express();
+
+app.get('/', (req,res)=>{
+    console.log("Hello from server")
+})
+
+module.exports = app;
